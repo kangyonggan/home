817933a68c172dbe0dc959c7cb8b7b6c018ab9dd
康永敢 <kangyonggan@gmail.com>
2016-10-20 17:47:38 +0800

    银行卡查询

--- src/main/java/com/kangyonggan/home/controller/ApiController.java
+++ src/main/java/com/kangyonggan/home/controller/ApiController.java
@@ -25,6 +25,7 @@ public class ApiController {
     private static final String PATH_ROOT = "api/";
     private static final String PATH_IP = PATH_ROOT + "ip";
     private static final String PATH_IDCARD = PATH_ROOT + "idcard";
+    private static final String PATH_CARDNUM = PATH_ROOT + "cardnum";
     private static final String PATH_RESULT = PATH_ROOT + "result";
 
     @Autowired
@@ -138,7 +139,7 @@ public class ApiController {
         category.setCode("all");
 
         model.addAttribute("category", category);
-        return PATH_IDCARD;
+        return PATH_CARDNUM;
     }
 
     /**
